0de24b3eff0b9f5f0e1e7af5ef8780d8d1c7725e
HUZAIFA NIZAM <121801411+huzaifanizam@users.noreply.github.com>
2023-08-13 00:06:29 +0500

    Grade calculator

---
+++ app.mjs
@@ -0,0 +1,13 @@
+let Grade = prompt("Enter your room Grade; " )
+
+if (Grade >= 90){
+    console.log("Grade Point is ; 4.0");
+}  else if  (Grade >= 80){
+    console.log("Grade Point is ; 3.3");
+}  else if  (Grade >= 70){
+    console.log("Grade Point is ; 2.3");
+}  else if  (Grade >= 60){
+    console.log("Grade Point is ; 1.3");
+}  else if  (Grade > 0){
+     console.log("Grade Point is ; 0 or Your are Fail");
+ } 
